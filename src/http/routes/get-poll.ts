af22b69d35bef6f29a07ef0b633d250bb595be8b
Arthur Machado <arthur.machado@iclubs.tech>
2024-02-08 00:56:48 -0300

    feat: return score when getting poll

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify"
 import z from "zod"
 import { prisma } from "@/lib/prisma"
+import { redis } from "@/lib/redis"
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -23,8 +24,36 @@ export async function getPoll(app: FastifyInstance) {
         }
       }
     })
+
+    if(!poll) {
+      return reply.status(400).send({ message: "Poll not found." })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = result.reduce((ranking, pollOption, index) => {
+      if(index % 2 === 0) {
+        const score = result[index + 1]
+
+        Object.assign(ranking, { [pollOption]: Number(score) })
+      }
+
+      return ranking
+    }, {} as Record<string, number>)
+
+    const pollObject = {
+      id: poll.id,
+      title: poll.title,
+      options: poll.options.map(option => {
+        return {
+          id: option.id,
+          title: option.title,
+          score: (option.id in votes) ? votes[option.id] : 0,
+        }
+      })
+    }
   
-    return reply.send({ poll })
+    return reply.send({ poll: pollObject })
   })
 }
 
